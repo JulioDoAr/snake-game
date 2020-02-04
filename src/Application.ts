e5fe8707539e2d381d0a09df2001c4bf06e09165
JULIO DOMINGUEZ ARJONA <0016445@insags.com>
2020-02-04 12:04:50 +0100

    UPDATE Refactored code

--- src/Application.ts
+++ src/Application.ts
@@ -1,3 +1,7 @@
+import {Snake} from './entities/Snake';
+import {Point} from './common/Point';
+import {Direction} from './common/Direction';
+
 class Application {
   private static readonly COLOR_BACKGROUND = 'black';
   private static readonly COLOR_SNAKE = 'lime';
@@ -8,8 +12,6 @@ class Application {
 
   private gs: number = 20;
   private boardSize: number = 20;
-  private xv: number = 0;
-  private yv: number = 0;
   private snake: Snake;
   private apple: Point;
   private direction: Direction = Direction.None;
@@ -40,7 +42,6 @@ class Application {
   }
 
   private repaintSnake() {
-    console.log(this.snake);
     this.context.fillStyle = Application.COLOR_SNAKE;
     for (var i = 0; i < this.snake.trail.length; i++) {
       this.context.fillRect(
@@ -79,80 +80,7 @@ class Application {
   }
 
   private keyPush(event: KeyboardEvent): void {
-    console.log(event.keyCode);
     this.direction = event.keyCode;
-  }
-}
-
-enum Direction {
-  None = 0,
-  N = 38,
-  S = 40,
-  E = 39,
-  W = 37
-}
-
-class Point {
-  public x: number;
-  public y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-
-  public equals(object: any) {
-    if (object instanceof Point) {
-      let aux = <Point>object;
-      return aux.x == this.x && aux.y == this.y;
-    }
-    return false;
-  }
-
-  public clone(): Point {
-    return new Point(this.x, this.y);
-  }
-}
-
-class Snake {
-  public trail: Point[] = [];
-  public tail: number = 5;
-  public headPosition: Point = new Point(10, 10);
-
-  constructor() {}
-
-  public grow(): void {
-    this.tail++;
-  }
-
-  public move(direction: Direction, boardSize: number): void {
-    switch (direction) {
-      case Direction.N:
-        this.headPosition.y--;
-        break;
-      case Direction.S:
-        this.headPosition.y++;
-        break;
-      case Direction.E:
-        this.headPosition.x++;
-        break;
-      case Direction.W:
-        this.headPosition.x--;
-        break;
-    }
-
-    if (this.headPosition.x < 0) {
-      this.headPosition.x = boardSize - 1;
-    }
-    if (this.headPosition.x > boardSize - 1) {
-      this.headPosition.x = 0;
-    }
-    if (this.headPosition.y < 0) {
-      this.headPosition.y = boardSize - 1;
-    }
-    if (this.headPosition.y > boardSize - 1) {
-      this.headPosition.y = 0;
-    }
   }
 }
 
